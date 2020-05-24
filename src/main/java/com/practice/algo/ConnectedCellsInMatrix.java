3ee83819cc5b6722015491b81c407d6a3106f4bd
sanjeev <sanjeev.srms@gmail.com>
2020-05-24 17:55:57 +0530

    Problem 5- Find Connected Cells in Matrix

---
+++ src/main/java/com/practice/algo/ConnectedCellsInMatrix.java
@@ -0,0 +1,55 @@
+package com.practice.algo;
+
+public class ConnectedCellsInMatrix {
+    static int size = 0;
+
+    public static void main(String[] args) {
+        int[][] mat = {{1, 1, 0, 0, 0}, {0, 1, 1, 0, 0}, {0, 0, 1, 0, 1}, {1, 0, 0, 0, 1}, {0, 1, 0, 1, 1}};
+        int rowSize = 5;
+        int colSize = 5;
+        System.out.println(getMaxConnectedCells(mat, rowSize, colSize));
+    }
+
+    private static int getMaxConnectedCells(int[][] mat, int rowSize, int colSize) {
+        int maxSize = 0;
+        boolean[][] tempMat = new boolean[rowSize][colSize];
+
+        for (int i = 0; i < rowSize; i++) {
+            for (int j = 0; j < colSize; j++) {
+                if (mat[i][j] == 1) {
+                    size=1;
+                    findMaxConnectedCells(mat, i, j, rowSize, colSize, tempMat);
+                    maxSize = Math.max(maxSize,size);
+                }
+            }
+        }
+        return maxSize;
+    }
+
+    private static void findMaxConnectedCells(int[][] mat, int i, int j, int rowSize, int colSize, boolean[][] tempMat) {
+
+        if (i > rowSize || j > colSize)
+            return;
+        tempMat[i][j] = true;
+
+        int[][] directions = {{-1, 1}, {0, 1}, {1, 1}, {1, 0}, {1, -1}, {0, -1}, {-1, -1}, {-1, 0}};
+        for (int p = 0; p < 8; p++) {
+
+            int ni = i + directions[p][0];
+            int nj = j + directions[p][1];
+
+            int checkVal = getNewCellVAlue(mat, ni, nj, rowSize, colSize);
+            if (checkVal > 0 && tempMat[ni][nj] == false) {
+                size++;
+                findMaxConnectedCells(mat, ni, nj, rowSize, colSize, tempMat);
+            }
+        }
+
+    }
+
+    private static int getNewCellVAlue(int[][] mat, int ni, int nj, int rowSize, int colSize) {
+        if (ni < 0 || ni >= rowSize || nj < 0 || nj >= colSize)
+            return 0;
+        return mat[ni][nj];
+    }
+}
